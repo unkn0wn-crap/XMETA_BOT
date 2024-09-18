8b5543838795a3820bd8fe4f57a334daf8074b06
corpse-x <arnav.gupta.0078@gmail.com>
2024-09-18 17:08:46 +0530

    small fix

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002221570986")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002092954715"))
 
 
