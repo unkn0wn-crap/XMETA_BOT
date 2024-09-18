4960d0c7b9222c5ae2f14fd496526bab3efe48d5
corpse-x <arnav.gupta.0078@gmail.com>
2024-09-18 17:25:18 +0530

    small fix

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -34,25 +34,25 @@ async def refunc(client, message):
         # Automatically process the file as a document
         if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
             # Automatically selecting document
-            await process_document(client, media.file_id, new_name)
+            await process_document(client, message.chat.id, media.file_id, new_name)
         elif file.media == MessageMediaType.AUDIO:
-            await process_audio(client, media.file_id, new_name)
+            await process_audio(client, message.chat.id, media.file_id, new_name)
 
 
-async def process_document(client, file_id, new_name):
+async def process_document(client, chat_id, file_id, new_name):
     # Code to handle document upload
     await client.send_document(
-        chat_id=client.chat.id,
+        chat_id=chat_id,
         document=file_id,
         file_name=new_name
     )
     # You can add other processing logic here as needed
 
 
-async def process_audio(client, file_id, new_name):
+async def process_audio(client, chat_id, file_id, new_name):
     # Code to handle audio upload if needed
     await client.send_audio(
-        chat_id=client.chat.id,
+        chat_id=chat_id,
         audio=file_id,
         file_name=new_name
     )
