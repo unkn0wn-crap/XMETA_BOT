ac9b90d4d44508a04f0a3f8f5cef2a07c0e13046
corpse-x <arnav.gupta.0078@gmail.com>
2024-09-18 17:05:10 +0530

    small fix

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 # Log & Force Channel Config
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002221570986")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002221570986"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002092954715"))
 
 
 # Mongo DB Database Config
